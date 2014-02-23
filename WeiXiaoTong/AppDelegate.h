7933836ab87747986b8b5012d61c926ec630fc0b
李世明 <lishiming@lishimingdeMac-mini.local>
2014-02-23 14:44:56 +0800

    Initial Commit

---
+++ WeiXiaoTong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeiXiaoTong
+//
+//  Created by 李世明 on 14-2-23.
+//  Copyright (c) 2014年 WXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeiXiaoTong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeiXiaoTong
+//
+//  Created by 李世明 on 14-2-23.
+//  Copyright (c) 2014年 WXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
